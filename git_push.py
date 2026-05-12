da3f71545ed879b22e1c810a03628c1c688d54d8
anilpedamallu <anilkumar9387@gmail.com>
2026-05-12 22:52:21 +0530

    update content

--- git_push.py
+++ git_push.py
@@ -1,15 +1,9 @@
 import subprocess
 
 try:
-    # Run git add .
     subprocess.run(["git", "add", "."], check=True)
-
-    # Run git commit
     subprocess.run(["git", "commit", "-m", "update content"], check=True)
-
-    # Run git push
     subprocess.run(["git", "push"], check=True)
-
     print("========== Git PUSH Success ==========")
 
 except subprocess.CalledProcessError:
